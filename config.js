fdff88824264f4af2694f729418bc6747e480f28
MohdAdn <adnanakbar716@gmail.com>
2025-08-06 19:02:42 +0530

    version 3.0 code added

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 console.log("this file is config.js version 1.0");
 console.log("version 2.0 of the config.js file");
-console.log("version 2.0 file updated")
+console.log("version 3.0 is of config.js");
--- src/index.js
+++ src/index.js
@@ -2,5 +2,4 @@ console.log("Hello git");
 console.log("I am in the index.js file version 1.0");
 console.log("Version 2.0 of the index file");
 console("version 2.0 is code is added in index file");
-
-console.log("version 2.0 is updated here");
+console.log("version 3.0 of index file");
